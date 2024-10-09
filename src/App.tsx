55f3c48b7a0fc9c60e837f887cb7533beae9e35d
01Barthez <kenwoubarthez@gmail.com>
2024-10-09 15:45:33 +0100

    little update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { ThemeProvider } from './hooks/useTheme'
-import { ModeToggle } from './components/custom/ModeToogle'
 
 const App: React.FC = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div>
         App
-
-        <ModeToggle/>
       </div>
     </ThemeProvider>
   )
--- src/components/custom/ModeToogle.tsx
+++ src/components/custom/ModeToogle.tsx
@@ -1,4 +1,5 @@
-import { Moon, Sun } from "lucide-react"
+import { BsFillMoonFill } from "react-icons/bs";
+import { BsSun } from "react-icons/bs";
 import { Button } from "../ui/button"
 import { useTheme } from "../../hooks/useTheme"
 import { useState } from "react"
@@ -6,24 +7,29 @@ import { useState } from "react"
 export function ModeToggle() {
   const { setTheme } = useTheme()
   const [isLight, setLight] = useState<boolean>(true)
+
   const changeTheme = () => {
-    setLight(prev => !prev)
-    {
-      isLight ? setTheme("light") : setTheme("dark")
-    }
-  }
+    setTheme(isLight ? "dark" : "light");
+    setLight((prev) => !prev);
+  };
+
   return (
     <>
       <Button
         variant='outline'
         size="icon"
         onClick={changeTheme}
+        className="hover:bg-gray-200"
       >
         {
           isLight ?
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <BsSun
+              className="h-[1.2rem] w-[1.2rem] scale-110 transition-all"
+            />
             :
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <BsFillMoonFill
+              className="absolute h-[1.2rem] w-[1.2rem] transition-all"
+            />
         }
       </Button>
     </>
