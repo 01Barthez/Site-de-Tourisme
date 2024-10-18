7b77292d6e809a3aee66d1983f1b78c7dbfbcd95
melvis8 <melvisnajela@gmail.com>
2024-10-18 14:37:07 +0100

    fixing a component

--- src/components/custom/Sections/TopAuthor.tsx
+++ src/components/custom/Sections/TopAuthor.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { TopAuthorsMock } from '../../../global/mocks/TopAuthors'
 import GridAuthorBox from '../GridAuthorBox'
+import { Button } from '../../ui/button'
 
 const TopAuthor: React.FC = () => {
   return (
@@ -9,19 +10,36 @@ const TopAuthor: React.FC = () => {
         <div className="flexContent">
           {/* Title header */}
           <h2 className="title text-center">
-          Top 10 author of the month
+            Top 10 author of the month
           </h2>
 
           {/* Paragraph header */}
           <p className="para text-center">
-          Rating based on customer reviews
+            Rating based on customer reviews
           </p>
         </div>
 
         {/* Slider Content */}
-      <div className="w-full mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 md:gap-3 lg:gap-4 place-items-center">
-       <GridAuthorBox TableToMap = {TopAuthorsMock} />
-      </div>
+        <div className="w-full mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 md:gap-3 lg:gap-4 place-items-center">
+          <GridAuthorBox TableToMap={TopAuthorsMock} />
+        </div>
+        
+        <div className='flex items-center gap-1 mx-auto'>
+          <Button
+            variant="outline"
+            className='mx-auto text-lg p-6 rounded-full flex items-center gap-2'
+          // onClick={selected3}
+          >
+            Show me more
+          </Button>
+
+          <Button
+            className='mx-auto text-lg p-6 rounded-full'
+          // onClick={selected3}
+          >
+            Become a host
+          </Button>
+        </div>
       </div>
     </section>
   )
